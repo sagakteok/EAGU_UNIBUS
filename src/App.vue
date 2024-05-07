084761e578d18e30d8e51aa135b9e52c92e43b50
sagakteok <maze77777@naver.com>
2024-05-07 22:02:06 +0900

    Add mdi icons at index.html

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,14 @@
 <template>
   <v-app>
     <test/>
-    <HelloWorld/>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
 import Test from "./components/test.vue";
 
 export default {
   name: "App",
-  components: {Test, HelloWorld},
+  components: {Test},
 };
 </script>
--- src/components/test.vue
+++ src/components/test.vue
@@ -1,29 +1,12 @@
 <template>
-  <v-app-bar>
-    <v-btn text @click="drawer = !drawer"> btn</v-btn>
-    <v-toolbar-title>Test Application</v-toolbar-title>
+  <v-app-bar :elevation="0">
+    <v-btn icon="mdi-dots-vertical" @click="drawer = !drawer" location="right"></v-btn>
+    <v-toolbar-title location="left">UNIBUS</v-toolbar-title>
   </v-app-bar>
 
-  <v-navigation-drawer v-model="drawer" location="left" color="black" app>
-    탭 안에 있을 UI
+  <v-navigation-drawer v-model="drawer" location="right" color="#006934" app>
+    목록
   </v-navigation-drawer>
-
-  <v-main style="max-height: 300px" fluid>
-    <v-card class="mx-auto" max-width="344">
-      <v-card-text>
-        <div>오늘의 단어</div>
-        <p class="display-1 text--primary">responsive web design</p>
-        <div class="text--primary">
-          화면의 크기가 자동으로 변하도록 만든 웹페이지 접근 기법을 말한다.
-        </div>
-      </v-card-text>
-      <v-card-actions>
-        <v-btn text color="deep-purple accent-4"> Learn More</v-btn>
-      </v-card-actions>
-    </v-card>
-  </v-main>
-
-
 </template>
 
 <style scoped>
