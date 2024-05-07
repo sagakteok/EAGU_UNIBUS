8702bad0a3ccc26a48a1f87d036ee5aa809754ac
sagakteok <maze77777@naver.com>
2024-05-07 16:28:50 +0900

    Add optimizeDeps

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  optimizeDeps: {
+    include: ['esm-dep > cjs-dep'],
+  },
   plugins: [vue()],
 })
