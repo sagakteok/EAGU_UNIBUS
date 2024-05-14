9669ee6a078575b1df4591049de9859a59c3395a
sagakteok <maze77777@naver.com>
2024-05-14 20:37:24 +0900

    Add clickable card in MainHome.vue

--- src/components/MainHome.vue
+++ src/components/MainHome.vue
@@ -1,5 +1,11 @@
 <template>
-
+  <v-card
+      class="mx-auto"
+      max-width="344"
+      subtitle="Same looks, no anchor"
+      title="Hover and click me"
+      link
+  ></v-card>
 </template>
 
 <style>
