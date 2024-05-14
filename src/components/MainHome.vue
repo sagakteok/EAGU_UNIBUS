a3ea2462668b3750256e5d071d18dfcee8b06d48
sagakteok <maze77777@naver.com>
2024-05-14 20:16:20 +0900

    Add navigation bar in app.vue

--- src/components/MainHome.vue
+++ src/components/MainHome.vue
@@ -1,14 +1,11 @@
-<script setup>
-</script>
-
 <template>
+
 </template>
 
 <style>
-  body {
-    background-image: url('../assets/MainHome.svg');
-    background-size: cover;
-    width: auto;
-    height: auto;
-  }
+
 </style>
+
+<script>
+
+</script>
