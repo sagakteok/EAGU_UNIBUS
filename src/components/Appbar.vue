b767e45ecbd66fdf06021055754cc15d3e1a3c00
sagakteok <maze77777@naver.com>
2024-05-21 22:02:50 +0900

    Appbar CSS remove

--- src/components/Appbar.vue
+++ src/components/Appbar.vue
@@ -12,35 +12,22 @@ function UNIBUS(name){
 <template>
   <v-app-bar class="app-bar" color="transparent" elevation="0" prominent>
     <v-btn @click="router.push('/')">
-      <v-img class="AppbarUNIBUS" :src="UNIBUS(fname)"/>
+      <v-img width="100px" :src="UNIBUS(fname)"/>
     </v-btn>
-    <v-btn @click="router.push('/shuttle')">
-      <div class="app-bar-text">셔틀버스</div>
+    <v-btn variant="text" @click="router.push('/shuttle')">
+      <div style="font-family: 'Inter-Bold', Helvetica; color: #FFFFFF; font-size: 20px; font-weight: 700;">셔틀버스</div>
     </v-btn>
-    <v-btn @click="router.push('/city')">
-      <div class="app-bar-text">시내버스</div>
+    <v-btn variant="text" @click="router.push('/city')">
+      <div style="font-family: 'Inter-Bold', Helvetica; color: #FFFFFF; font-size: 20px; font-weight: 700;">시내버스</div>
     </v-btn>
-    <v-btn @click="router.push('/news')">
-      <div class="app-bar-text">소식</div>
+    <v-btn variant="text" @click="router.push('/news')">
+      <div style="font-family: 'Inter-Bold', Helvetica; color: #FFFFFF; font-size: 20px; font-weight: 700;">소식</div>
     </v-btn>
     <v-btn href="https://www.dju.ac.kr/">
-      <div class="app-bar-text">대전대학교</div>
+      <div style="font-family: 'Inter-Bold', Helvetica; color: #FFFFFF; font-size: 20px; font-weight: 700;">대전대학교</div>
     </v-btn>
   </v-app-bar>
 </template>
-
-<style>
-.AppbarUNIBUS{
-  width: 100px;
-}
-
-.app-bar-text {
-  font-family: "Inter-Bold", Helvetica;
-  color: #FFFFFF;
-  font-size: 20px;
-  font-weight: 700;
-}
-</style>
 
 <script>
 
