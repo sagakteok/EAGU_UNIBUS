a7416a3e4b7a74c1dd1e33a77d413d9b2acafd1c
sagakteok <maze77777@naver.com>
2024-05-23 01:12:26 +0900

    Remove Appbar outline

--- src/components/Appbar.vue
+++ src/components/Appbar.vue
@@ -1,18 +1,18 @@
 <template>
   <v-app-bar color="transparent" elevation="0" prominent>
-    <v-btn @click="gotomain()">
-      <v-img width="100px" :src="UNIBUS"/>
+    <v-btn style="outline: none;" @click="gotomain()">
+      <v-img style="width: 100px;" :src="UNIBUS"/>
     </v-btn>
-    <v-btn variant="text" @click="gotoshuttle()">
+    <v-btn variant="text" style="outline: none;" @click="gotoshuttle()">
       <div style="font-family: 'Inter-Bold', Helvetica; color: #FFFFFF; font-size: 20px; font-weight: 700;">셔틀버스</div>
     </v-btn>
-    <v-btn variant="text" @click="gotocity()">
+    <v-btn variant="text" style="outline: none;" @click="gotocity()">
       <div style="font-family: 'Inter-Bold', Helvetica; color: #FFFFFF; font-size: 20px; font-weight: 700;">시내버스</div>
     </v-btn>
-    <v-btn variant="text" @click="gotonews()">
+    <v-btn variant="text" style="outline: none;" @click="gotonews()">
       <div style="font-family: 'Inter-Bold', Helvetica; color: #FFFFFF; font-size: 20px; font-weight: 700;">소식</div>
     </v-btn>
-    <v-btn href="https://www.dju.ac.kr/">
+    <v-btn style="outline: none;" href="https://www.dju.ac.kr/">
       <div style="font-family: 'Inter-Bold', Helvetica; color: #FFFFFF; font-size: 20px; font-weight: 700;">대전대학교</div>
     </v-btn>
   </v-app-bar>
