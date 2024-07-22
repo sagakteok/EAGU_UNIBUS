1450a2a10daa6a351a659e59b5b633d6641499d2
minjin kim <sajajin115@gmail.com>
2024-07-22 21:46:08 +0900

    js file -> json file

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT;
 
 app.use(express.static(path.join(__dirname,"../../dist")));
 app.listen(port, () => {
