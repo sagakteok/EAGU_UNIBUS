909e404915ffd4206cfe031e8fe9e897258f274b
minjin kim <sajajin115@gmail.com>
2024-07-23 16:13:46 +0900

    js file -> json file

--- backend/src/controllers/renderController.js
+++
@@ -1,6 +0,0 @@
-const path = require('path');
- const renderController = (req, res) => {
-     res.sendFile(path.join(__dirname,"../../../dist","index.html"));
-};
-
- module.exports = renderController
